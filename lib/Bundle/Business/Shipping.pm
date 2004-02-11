04e7d44117ba4957428a6f59fc8ea4fd707d1a94
Daniel Browning <db@kavod.com>
2004-02-11 15:50:21 +0000

    Import of DBROWNING/Bundle-Business-Shipping-1.07 from CPAN.
    gitpan-cpan-distribution: Bundle-Business-Shipping
    gitpan-cpan-version:      1.07
    gitpan-cpan-path:         DBROWNING/Bundle-Business-Shipping-1.07.tar.gz
    gitpan-cpan-author:       DBROWNING
    gitpan-cpan-maturity:     released

---
+++ lib/Bundle/Business/Shipping.pm
@@ -0,0 +1,78 @@
+# Bundle::Business::Shipping - a Bundle for Business::Shipping.
+# 
+# $Id $
+# 
+# Copyright (c) 2003-2004 Kavod Technologies, Dan Browning. All rights reserved. 
+# 
+# Licensed under the GNU Public Licnese (GPL).  See COPYING for more info.
+# 
+
+package Bundle::Business::Shipping;
+
+$VERSION = "1.07";
+
+use strict;
+
+1;
+__END__
+
+=head1 NAME
+
+Bundle::Business::Shipping - a Bundle for Business::Shipping.
+
+=head1 SYNOPSIS
+
+C<perl -MCPAN -e install Bundle::SQL::Translator>
+
+=head1 DESCRIPTION
+
+C<Bundle::Business::Shipping> provides a bundle to install all 
+C<Business::Shiping> prerequisites.
+
+=head1 CONTENTS
+
+Archive::Zip
+
+Bundle::DBD::CSV
+
+Cache::FileCache
+
+Class::MethodMaker
+
+Config::IniFiles 
+
+Crypt::SSLeay
+
+Data::Dumper
+
+Devel::Required     0.03
+
+Error
+
+LWP::UserAgent
+
+Math::BaseCnv
+
+XML::DOM
+
+XML::Simple         2.05
+
+=head1 SUPPORT
+
+C<Bundle::Business::Shipping> is supported by the author.
+
+=head1 SEE ALSO
+
+L<Business::Shipping>
+
+=head1 AUTHOR
+
+ Dan Browning         <db@kavod.com>
+ Kavod Technologies   http://www.kavod.com
+
+=head1 COPYRIGHT AND LICENCE
+
+Copyright (c) 2003-2004 Kavod Technologies, Dan Browning. All rights reserved. 
+Licensed under the GNU Public Licnese (GPL).  See COPYING for more info.
+
+=cut
